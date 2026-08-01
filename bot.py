a670bcb6a83c8fd7557ce33657a751e0223e45f3
welratu <welratu0@gmail.com>
2026-08-02 00:25:39 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,20 @@
-from telegram import Update
-from telegram.ext import Application, MessageHandler, ContextTypes, filters
+
+import re
 
 TOKEN = "8062489806:AAFSWWiJGU3gglcLope0LdYhqRGbMR3Y6VM"
 
-TRIGGERS = {
-    "лавров",
-    "лаврова",
-    "лаврову",
-}
+from telegram import Update
+from telegram.ext import Application, MessageHandler, ContextTypes, filters
 
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not update.message or not update.message.text:
         return
 
-    text = update.message.text.lower().strip()
+    text = update.message.text.lower()
 
-    if text in TRIGGERS:
+    if re.search(r"\bлавров(?:а|у)?\b", text):
         await update.message.reply_text("царствие ему небесное")
+
 
 def main():
     app = Application.builder().token(TOKEN).build()
@@ -24,6 +22,7 @@ def main():
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
 
     app.run_polling()
+
 
 if __name__ == "__main__":
     main()
