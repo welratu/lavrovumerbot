e4cf2ce81fea8967c3db6e73bc9af52f0f578379
welratu <welratu0@gmail.com>
2026-08-02 00:51:03 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,6 @@ from telegram.ext import Application, MessageHandler, ContextTypes, filters
 
 TOKEN = "8062489806:AAFSWWiJGU3gglcLope0LdYhqRGbMR3Y6VM"
 
-import re
-
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not update.message or not update.message.text:
         return
@@ -28,3 +26,15 @@ async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text(
             "завр приваленко виддав життя за украину и його йобнуло шахидом 🕯️🕯️🥀 царствие йому небесное"
         )
+
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
+
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
